197d7820eaf7f1c88224a321a1d01d2eff955ccf
Miguel Rentes <miguel.rentes@gmail.com>
2015-08-28 16:30:30 +0100

    Refactored spec expectations

--- spec/Behavioral/Chain of Responsibility/chain_of_responsibility_spec.rb
+++ spec/Behavioral/Chain of Responsibility/chain_of_responsibility_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../../../spec/spec_helper'
 require_relative '../../../Behavioral/Chain of Responsibility/chain_of_responsibility'
 
-describe ChainOfResponsibility, '#message_from_debug_logger' do
-  it 'returns \'output test.\' on stdout from the logger debug' do
+describe ChainOfResponsibility do
+  it "returns output test on stdout from the logger debbug" do
     chain = ChainOfResponsibility.new.create_chain
     output = capture_stdout { chain.message('Entering function y.', Logger::DEBUG) }
     output.should include 'Sending to stdout: Entering function y.'
+    output = capture_stdout { chain.message('Step1 completed.', Logger::NOTICE) }
+    output.should include 'Sending via e-mail: Step1 completed.'
+    output = capture_stdout { chain.message('An error has occured.', Logger::ERR) }
+    output.should include 'Sending to stdout: An error has occured'
   end
 end
