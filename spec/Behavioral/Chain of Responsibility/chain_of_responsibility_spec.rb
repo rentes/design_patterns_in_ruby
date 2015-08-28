bb6dcdd70f3aaa3801f55b684b9162dc522d3db6
Miguel Rentes <miguel.rentes@gmail.com>
2015-08-28 16:53:31 +0100

    Refactors code for removing rubocop offenses

--- spec/Behavioral/Chain of Responsibility/chain_of_responsibility_spec.rb
+++ spec/Behavioral/Chain of Responsibility/chain_of_responsibility_spec.rb
@@ -2,13 +2,16 @@ require_relative '../../../spec/spec_helper'
 require_relative '../../../Behavioral/Chain of Responsibility/chain_of_responsibility'
 
 describe ChainOfResponsibility do
-  it "returns output test on stdout from the logger debbug" do
+  it 'returns output test on stdout from the logger debbug' do
     chain = ChainOfResponsibility.new.create_chain
-    output = capture_stdout { chain.message('Entering function y.', Logger::DEBUG) }
+    output =
+      capture_stdout { chain.message('Entering function y.', Logger::DEBUG) }
     output.should include 'Sending to stdout: Entering function y.'
-    output = capture_stdout { chain.message('Step1 completed.', Logger::NOTICE) }
+    output =
+      capture_stdout { chain.message('Step1 completed.', Logger::NOTICE) }
     output.should include 'Sending via e-mail: Step1 completed.'
-    output = capture_stdout { chain.message('An error has occured.', Logger::ERR) }
+    output =
+      capture_stdout { chain.message('An error has occured.', Logger::ERR) }
     output.should include 'Sending to stdout: An error has occured'
   end
 end
